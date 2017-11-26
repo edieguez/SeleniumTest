364325c73b01a910d57553be0b5684b8e5245003
edieguez <dieguezemmanuel@gmail.com>
2017-11-26 07:34:48 -0600

    Added a simple test for Google chrome

--- src/main/java/com/artema/App.java
+++ src/main/java/com/artema/App.java
@@ -1,10 +1,22 @@
 package com.artema;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Set the Chrome web driver location
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
     }
 }
